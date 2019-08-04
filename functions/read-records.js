c4746d42facd88bf4944625b903549b70b811d69
Bullal <51942639+bullal@users.noreply.github.com>
2019-08-04 12:50:01 +0300

    try the fix

--- functions/read-records.js
+++ functions/read-records.js
@@ -13,12 +13,9 @@ exports.handler = (event, context, callback) => {
     /* parse the string body into a useable JS object */
     const data = JSON.parse(event.body)
     console.log('Function `todo-create` invoked', data)
-    const searchItem = {
-      data: data
-    }
     // get users to get the role of the user
     /* construct the fauna query */
-    client.query(q.Paginate(q.Match(q.Ref(`indexes/${searchItem.collection}_by_product`), searchItem.product)))
+    client.query(q.Paginate(q.Match(q.Ref(`indexes/${data.collection}_by_product`), data.product)))
     .then(response => response.data.map(record => client.query(q.Get(record))))
     .then((response) => {
       console.log('success', response)
